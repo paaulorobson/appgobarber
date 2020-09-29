2b507a1ee6fc86db8bb3307963db3a3dcb8ed54a
paaulorobson <p.robson085@gmail.com>
2020-09-28 21:23:56 -0300

    D - Navegaçao entre telas login/cadastro.

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Image, ScrollView, KeyboardAvoidingView, Platform} from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
+import {useNavigation} from '@react-navigation/native'
 
 import Logo from '../../assets/logo.png'
 
@@ -10,6 +11,8 @@ import Button from '../../components/Button'
 import {Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText} from './styles'
 
 const SignIn: React.FC = () => {
+
+    const navigation = useNavigation()
     return (
         <>
             <KeyboardAvoidingView 
@@ -24,7 +27,7 @@ const SignIn: React.FC = () => {
                 <ScrollView contentContainerStyle={{flex: 1}}>    
                     <Container>
                         <Image source={Logo}/>
-                        <Title>Faça seu Login</Title>
+                        <Title>Faça seu Logon</Title>
 
                         <Input name="email" icon="mail" placeholder="E-mail"/>
                         <Input name="password" icon="lock" placeholder="Senha"/>
@@ -37,7 +40,7 @@ const SignIn: React.FC = () => {
                     </Container>
                 </ScrollView>
 
-                <CreateAccountButton onPress = {() => {}}>
+                <CreateAccountButton onPress = {() => navigation.navigate('SignUp')}>
                     <Icon name="log-in" size={20} color="#ff9000"/>
                     <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
                 </CreateAccountButton>
--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
-import {Container} from './styles'
+import {Image, ScrollView, KeyboardAvoidingView, Platform} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import {useNavigation} from '@react-navigation/native'
+
+import Logo from '../../assets/logo.png'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import {Container, Title, BackToSignIn, BackToSignInText} from './styles'
 
 const SignUp: React.FC = () => {
+
+    const navigation = useNavigation()
     return (
-        <Container/>
+        <>  
+            <KeyboardAvoidingView 
+                style= {
+                    {
+                        flex: 1
+                    }
+                }    
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+                >
+                <ScrollView contentContainerStyle={{flex: 1}}>    
+                    <Container>
+                        <Image source={Logo}/>
+                        <Title>Crie sua conta</Title>
+                        <Input name="name" icon="user" placeholder="Nome" />
+                        <Input name="email" icon="mail" placeholder="E-mail"/>
+                        <Input name="password" icon="lock" placeholder="Senha"/>
+
+                        <Button onPress= {() => {console.log('Pronto')}} >Cadastrar</Button>
+
+                    </Container>
+                </ScrollView>
+
+                <BackToSignIn onPress = {() => navigation.goBack()}>
+                    <Icon name="arrow-left" size={20} color="#fff"/>
+                    <BackToSignInText>Voltar para Logon</BackToSignInText>
+                </BackToSignIn>
+            </KeyboardAvoidingView>
+        </>
 
     )
 }
